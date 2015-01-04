424b5cf290a3c781b60d7e954d1e9d0ba3660159
Boris Fritscher <boris@fritscher.ch>
2015-01-04 12:24:04 +0100

    update to weekly deal stegpc

--- stegpcdailydeal.php
+++ stegpcdailydeal.php
@@ -1,13 +1,6 @@
 <?php
 $html =  file_get_contents('http://www.steg-electronics.ch/fr/Default.aspx');
-preg_match( '/<div.*?url\((.*?)\).*?href="(.*?)"/i', $html, $matches);
-
-$title = date('d-m-Y');
-$link = $matches[2];
-$description = '<div><a href="' . $matches[2] . '"><img src="' . $matches[1] . '"/></a></div>';
-
-$date =  date('D, d M Y H:i:s O', mktime(6, 0, 0));
-$id =  $title;
+preg_match_all( "/<div.*?url\((.*?)\).*Weekly-Deals','Klick','(.*?)'.*?href=\"(.*?)\"/i", $html, $matches);
 
 echo '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
@@ -19,6 +12,16 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
         <link>http://www.steg-electronics.ch/fr/Default.aspx</link>
         <lastBuildDate><?php echo $date;?></lastBuildDate>
         <pubDate><?php echo $date;?></pubDate>
+<?php 
+$i=0;
+while( $i < count($matches[0])) {
+  $id = $matches[2][$i];
+  $title = str_replace('-', ' ', $id);
+  $link = $matches[3][$i];
+  $description = '<div><a href="' . $matches[3][$i] . '"><img src="' . $matches[1][$i] . '"/></a></div>';
+  $date =  date('D, d M Y H:i:s O', mktime(6, 0, 0));
+  $i++;
+?>
         <item>
                 <title><?php echo $title;?></title>
                 <description><?php echo htmlentities($description); ?></description>
@@ -26,6 +29,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
                 <guid><?php echo $id;?></guid>
                 <pubDate><?php echo $date;?></pubDate>
         </item>
+<?php } ?>        
 </channel>
 </rss>
 
