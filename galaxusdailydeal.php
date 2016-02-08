bc4dcc09eb453142df7776228c4d201e1a46c228
Boris Fritscher <boris@fritscher.ch>
2016-02-08 14:57:03 +0100

    fix galaxus feeds

--- galaxusdailydeal.php
+++ galaxusdailydeal.php
@@ -1,25 +1,27 @@
 <?php
-$html =  file_get_contents('https://www.galaxus.ch/fr/LiveShopping');
-preg_match( '/<article class="product dday dday-big.*?>(.*?)<form/is', $html, $matches);
+require('core.php');
+
+$html =  get_web_page_content('https://www.galaxus.ch/fr/LiveShopping');
+preg_match( '/<article class=".*?daily-offer.*?>(.*?)<\/article/is', $html, $matches);
 
 $link = "https://www.galaxus.ch";
 $title = date('d-m-Y');
 $description = $matches[1];
-
-preg_match('/url\((.*?)\)/', $description, $imgs);
+preg_match('/ src="(.*?)"/', $description, $imgs);
+$description = preg_replace('/^.*?<\/noscript>/s', '', $description);
 $description = preg_replace( '/href="(.*?)"/', 'href="' . $link . '${1}"', $description);
-$description = '<img src="'.$link. $imgs[1] .'" />' . $description;
-$description = preg_replace('/<div class="stock">.*?<\/div>/s', '', $description);
+$description = '<img src="'. $imgs[1] .'" />' . $description;
+$description = preg_replace('/<div class="daily-offer__stock.*?<\/div>/s', '<div>', $description);
 
 preg_match('/class="overlay".*?href="(.*?)"/s', $description, $links);
 $link = $links[1];
 $description = preg_replace('/class="overlay"(.*?)></s', 'class="overlay"${1}>Page produit<', $description);
-preg_match( '/<div.*?class="date.*?(\d+)<\//is', $description, $date_match);
+preg_match( '/<div.*?class="daily-offer__date.*?(\d+)<\//is', $description, $date_match);
+
 
 $date =  date('D, d M Y H:i:s O', mktime(6, 0, 0));
 $id =  $title;
 
-echo '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
 
 <rss version="2.0">
