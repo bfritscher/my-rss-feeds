83fb9f7c9c9b5b41032328863be088cf3d9130dd
Boris Fritscher <boris@fritscher.ch>
2017-06-09 11:41:38 +0200

    full-txt devto articles linked in twitter feed

---
+++ twitter.devto.php
@@ -0,0 +1,12 @@
+<?php
+require('core.php');
+
+$rss =  get_web_page_content('https://twitrss.me/twitter_user_to_rss/?user=ThePracticalDev');
+preg_match_all( '/<a href="(https:\/\/dev\.to\/.*?)">.*?<\/a>/is', $rss, $matches);
+for($i=0; $i < count($matches[0]); $i++){
+	$html =  get_web_page_content($matches[1][$i]);
+	preg_match( '/(<div class="body">.*?)<div class="comments-container-container"/is', $html, $match);
+	$rss = str_replace($matches[0][$i], $match[1], $rss);
+}
+print $rss;
+
