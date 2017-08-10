adc26e92b867e5cc6b13fd3eb268e463c4c5a4f1
Boris Fritscher <boris@fritscher.ch>
2017-08-11 00:52:58 +0200

    fix galaxus rss

--- digitecdailydeal.php
+++ digitecdailydeal.php
@@ -1,23 +1,23 @@
 <?php
+date_default_timezone_set('Europe/Zurich');
 require('core.php');
 
 $html =  get_web_page_content('https://www.digitec.ch/fr/LiveShopping');
-preg_match( '/<article class=".*?daily-offer.*?>(.*?)<\/article/is', $html, $matches);
+preg_match( '/.*?daily-offer.*?>(.*?)<\/article/is', $html, $matches);
 
 $link = "https://www.digitec.ch";
 $title = date('d-m-Y');
 $description = $matches[1];
-$description = preg_replace('/<ul class="product-recommender.*<\/ul>/s', '', $description);
-preg_match('/ data-src="(.*?)"/', $description, $imgs);
-$description = preg_replace('/^.*?<\/noscript>/s', '', $description);
-$description = preg_replace( '/href="(.*?)"/', 'href="' . $link . '${1}"', $description);
-$description = '<img src="'. $imgs[1] .'" />' . $description;
-$description = preg_replace('/<div class="daily-offer__stock.*?<\/div>/s', '<div>', $description);
 
-preg_match('/class="overlay".*?href="(.*?)"/s', $description, $links);
+preg_match('/ data-src="(.*?)"/', $description, $imgs);
+$description = preg_replace( '/href="(.*?)"/', 'href="' . $link . '${1}"', $description);
+
+
+preg_match('/class="overlay.*?href="(.*?)"/s', $description, $links);
 $link = $links[1];
-$description = preg_replace('/class="overlay"(.*?)></s', 'class="overlay"${1}>Page produit<', $description);
-preg_match( '/<div.*?class="daily-offer__date.*?(\d+)<\//is', $description, $date_match);
+preg_match( '/<div.*?class="daily-offer-new-date__day.*?(\d+)<\//is', $description, $date_match);
+preg_match('/(<div class="product-content">.*)<div class="product-buttons">/is', $description, $match);
+$description = '<img src="'. $imgs[1] .'" />' . $match[1];
 
 
 $date =  date('D, d M Y H:i:s O', mktime(6, 0, 0));
@@ -36,7 +36,10 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 <?php if ($date_match[1] == date('d')): ?>
         <item>
                 <title><?php echo $title;?></title>
-                <description><?php echo htmlentities($description); ?></description>
+                <description><![CDATA[
+<?php echo $description; ?>
+  ]]>
+</description>
                 <link><?php echo $link;?></link>
                 <guid><?php echo $id;?></guid>
                 <pubDate><?php echo $date;?></pubDate>
--- galaxusdailydeal.php
+++ galaxusdailydeal.php
@@ -2,22 +2,21 @@
 require('core.php');
 
 $html =  get_web_page_content('https://www.galaxus.ch/fr/LiveShopping');
-preg_match( '/<article class=".*?daily-offer.*?>(.*?)<\/article/is', $html, $matches);
+preg_match( '/.*?daily-offer.*?>(.*?)<\/article/is', $html, $matches);
 
 $link = "https://www.galaxus.ch";
 $title = date('d-m-Y');
 $description = $matches[1];
-$description = preg_replace('/<ul class="product-recommender.*<\/ul>/s', '', $description);
-preg_match('/ data-src="(.*?)"/', $description, $imgs);
-$description = preg_replace('/^.*?<\/noscript>/s', '', $description);
-$description = preg_replace( '/href="(.*?)"/', 'href="' . $link . '${1}"', $description);
-$description = '<img src="'. $imgs[1] .'" />' . $description;
-$description = preg_replace('/<div class="daily-offer__stock.*?<\/div>/s', '<div>', $description);
 
-preg_match('/class="overlay".*?href="(.*?)"/s', $description, $links);
+preg_match('/ data-src="(.*?)"/', $description, $imgs);
+$description = preg_replace( '/href="(.*?)"/', 'href="' . $link . '${1}"', $description);
+
+
+preg_match('/class="overlay.*?href="(.*?)"/s', $description, $links);
 $link = $links[1];
-$description = preg_replace('/class="overlay"(.*?)></s', 'class="overlay"${1}>Page produit<', $description);
-preg_match( '/<div.*?class="daily-offer__date.*?(\d+)<\//is', $description, $date_match);
+preg_match( '/<div.*?class="daily-offer-new-date__day.*?(\d+)<\//is', $description, $date_match);
+preg_match('/(<div class="product-content">.*)<div class="product-buttons">/is', $description, $match);
+$description = '<img src="'. $imgs[1] .'" />' . $match[1];
 
 
 $date =  date('D, d M Y H:i:s O', mktime(6, 0, 0));
@@ -35,7 +34,10 @@ $id =  $title;
 <?php if ($date_match[1] == date('d')): ?>
         <item>
                 <title><?php echo $title;?></title>
-                <description><?php echo htmlentities($description); ?></description>
+                <description><![CDATA[
+<?php echo $description; ?>
+  ]]>
+</description>
                 <link><?php echo $link;?></link>
                 <guid><?php echo $id;?></guid>
                 <pubDate><?php echo $date;?></pubDate>
@@ -43,9 +45,3 @@ $id =  $title;
 <?php endif;?>
 </channel>
 </rss>
-
-
-
-
-
-
