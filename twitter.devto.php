42312b486ccac4ad6d62b4da373b1798805cd260
Boris Fritscher <boris@fritscher.ch>
2020-07-11 19:02:06 +0200

    fix devto expand

--- twitter.devto.php
+++ twitter.devto.php
@@ -2,10 +2,10 @@
 require('core.php');
 
 $rss =  get_web_page_content('https://rsshub.app/twitter/user/ThePracticalDev');
-preg_match_all( '/<a href="(https:\/\/dev\.to\/.*?)">.*?<\/a>/is', $rss, $matches);
+preg_match_all( '/<a href="(https:\/\/dev\.to\/.*?)" t.*?<\/a>/is', $rss, $matches);
 for($i=0; $i < count($matches[0]); $i++){
 	$html =  get_web_page_content($matches[1][$i]);
-	preg_match( '/(<div class="body" data.*?>.*?)<div class="comments-container-container"/is', $html, $match);
+	preg_match( '/(<article.*?>.*?<\/article>)/is', $html, $match);
 	$rss = str_replace($matches[0][$i], $match[1], $rss);
 }
 print $rss;
